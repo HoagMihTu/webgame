348e39b9cdc10dbac23d38c4b331250afbc12475
tu <hoangtu2000718@gmail.com>
2023-04-20 22:23:23 +0700

    test deploy

--- src/components/views/LandingPage/LandingPage.tsx
+++ src/components/views/LandingPage/LandingPage.tsx
@@ -8,12 +8,12 @@ function LandingPage() {
         <h1>Demo game</h1>
         <div className="list-row">
           <Link to="/game1" className='text'>
-            Game 1: chịu
+            Game 1: Đếm đến 5
           </Link>
         </div>
         <div className='list-row'>
           <Link to="/game2" className='text'>
-            Game 2: chịu
+            Game 2: Đếm đến 5 trong khung
           </Link>
         </div>
         <div className='list-row'>
